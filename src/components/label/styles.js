4462ce0b4a380ad3b32cdfd516d015d88186d830
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2024-03-17 20:21:03 +0530

    improve

--- src/components/label/styles.js
+++ src/components/label/styles.js
@@ -1,77 +1,64 @@
 import Box from '@mui/material/Box';
 import { alpha, styled } from '@mui/material/styles';
 
-// ----------------------------------------------------------------------
+const variantStyles = {
+  filled: ({ theme, ownerState }) => ({
+    color: theme.palette.mode === 'light' ? theme.palette.common.white : theme.palette.grey[800],
+    backgroundColor: theme.palette.text.primary,
+    border: 'none',
+  }),
+  outlined: ({ theme, ownerState }) => ({
+    backgroundColor: 'transparent',
+    color: theme.palette.text.primary,
+    border: `2px solid ${theme.palette.text.primary}`,
+  }),
+  soft: ({ theme, ownerState }) => ({
+    color: theme.palette.text.secondary,
+    backgroundColor: alpha(theme.palette.grey[500], 0.16),
+    border: 'none',
+  }),
+};
 
-export const StyledLabel = styled(Box)(({ theme, ownerState }) => {
-  const lightMode = theme.palette.mode === 'light';
+const colorStyles = ({ theme, ownerState }) => ({
+  color: theme.palette[ownerState.color].contrastText,
+  backgroundColor: theme.palette[ownerState.color].main,
+  border: `2px solid ${theme.palette[ownerState.color].main}`,
+});
 
-  const filledVariant = ownerState.variant === 'filled';
+const softColorStyles = ({ theme, ownerState }) => ({
+  color: theme.palette[ownerState.color][theme.palette.mode === 'light' ? 'dark' : 'light'],
+  backgroundColor: alpha(theme.palette[ownerState.color].main, 0.16),
+  border: 'none',
+});
 
-  const outlinedVariant = ownerState.variant === 'outlined';
-
-  const softVariant = ownerState.variant === 'soft';
-
-  const defaultStyle = {
-    ...(ownerState.color === 'default' && {
-      // FILLED
-      ...(filledVariant && {
-        color: lightMode ? theme.palette.common.white : theme.palette.grey[800],
-        backgroundColor: theme.palette.text.primary,
-      }),
-      // OUTLINED
-      ...(outlinedVariant && {
-        backgroundColor: 'transparent',
-        color: theme.palette.text.primary,
-        border: `2px solid ${theme.palette.text.primary}`,
-      }),
-      // SOFT
-      ...(softVariant && {
-        color: theme.palette.text.secondary,
-        backgroundColor: alpha(theme.palette.grey[500], 0.16),
-      }),
-    }),
-  };
-
-  const colorStyle = {
-    ...(ownerState.color !== 'default' && {
-      // FILLED
-      ...(filledVariant && {
-        color: theme.palette[ownerState.color].contrastText,
-        backgroundColor: theme.palette[ownerState.color].main,
-      }),
-      // OUTLINED
-      ...(outlinedVariant && {
-        backgroundColor: 'transparent',
-        color: theme.palette[ownerState.color].main,
-        border: `2px solid ${theme.palette[ownerState.color].main}`,
-      }),
-      // SOFT
-      ...(softVariant && {
-        color: theme.palette[ownerState.color][lightMode ? 'dark' : 'light'],
-        backgroundColor: alpha(theme.palette[ownerState.color].main, 0.16),
-      }),
-    }),
-  };
+const StyledLabel = styled(Box)(({ theme, ownerState }) => {
+  const variantStyle = variantStyles[ownerState.variant];
+  const colorStyle = ownerState.color !== 'default' ? (ownerState.variant === 'soft' ? softColorStyles : colorStyles) : {};
 
   return {
-    height: 24,
-    minWidth: 24,
-    lineHeight: 0,
-    borderRadius: 6,
-    cursor: 'default',
-    alignItems: 'center',
-    whiteSpace: 'nowrap',
-    display: 'inline-flex',
-    justifyContent: 'center',
-    textTransform: 'capitalize',
-    padding: theme.spacing(0, 0.75),
-    fontSize: theme.typography.pxToRem(12),
-    fontWeight: theme.typography.fontWeightBold,
-    transition: theme.transitions.create('all', {
-      duration: theme.transitions.duration.shorter,
-    }),
-    ...defaultStyle,
-    ...colorStyle,
+    ...commonStyles,
+    ...variantStyle(theme, ownerState),
+    ...colorStyle(theme, ownerState),
   };
 });
+
+const commonStyles = {
+  height: 24,
+  minWidth: 24,
+  lineHeight: 0,
+  borderRadius: 6,
+  cursor: 'default',
+  alignItems: 'center',
+  whiteSpace: 'nowrap',
+  display: 'inline-flex',
+  justifyContent: 'center',
+  textTransform: 'capitalize',
+  padding: theme.spacing(0, 0.75),
+  fontSize: theme.typography.pxToRem(12),
+  fontWeight: theme.typography.fontWeightBold,
+  transition: theme.transitions.create('all', {
+    duration: theme.transitions.duration.shorter,
+  }),
+};
+
+export default StyledLabel;
