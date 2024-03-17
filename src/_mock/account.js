8b1ee4601d9d0e28cc1eaab14502ba51bafac185
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2024-03-17 20:20:54 +0530

    improve

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,7 +1,18 @@
 // ----------------------------------------------------------------------
 
-export const account = {
-  displayName: 'Jaydon Frankie',
-  email: 'demo@minimals.cc',
-  photoURL: '/assets/images/avatars/avatar_25.jpg',
+type Account = {
+  displayName: string;
+  email: string;
+  photoURL: string;
 };
+
+export class AccountClass {
+  constructor(
+    public displayName: string = 'Jaydon Frankie',
+    public email: string = 'demo@minimals.cc',
+    public photoURL: string = '/assets/images/avatars/avatar_25.jpg'
+  ) {}
+}
+
+export const account: Account = new AccountClass();
+
