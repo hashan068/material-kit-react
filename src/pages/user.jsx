ba0fec20f38857597df033fb83261f176d2b2406
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2024-03-17 20:20:57 +0530

    improve

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,17 +1,14 @@
 import { Helmet } from 'react-helmet-async';
+import UserView from 'src/sections/user/view';
 
-import { UserView } from 'src/sections/user/view';
+const UserPage = () => (
+  <>
+    <Helmet>
+      <title>User | Minimal UI</title>
+    </Helmet>
+    <UserView />
+  </>
+);
 
-// ----------------------------------------------------------------------
+export default UserPage;
 
-export default function UserPage() {
-  return (
-    <>
-      <Helmet>
-        <title> User | Minimal UI </title>
-      </Helmet>
-
-      <UserView />
-    </>
-  );
-}
