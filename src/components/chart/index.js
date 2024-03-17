e0fcd2be4846d1e3444165b6ab79ce392b184b41
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2024-03-17 20:20:57 +0530

    improve

--- src/components/chart/index.js
+++ src/components/chart/index.js
@@ -1,5 +1,10 @@
 // ----------------------------------------------------------------------
 
-export { default } from './chart';
+// Import the default export from './chart' and re-export it
+import Chart from './chart';
+export { Chart as default };
 
-export { default as useChart } from './use-chart';
+// Import the default export from './use-chart' and re-export it
+import useChart from './use-chart';
+export { useChart as default };
+
