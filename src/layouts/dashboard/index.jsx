7bb53187719125f95afb55616c3d302a614660d8
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2024-03-17 20:21:01 +0530

    improve

--- src/layouts/dashboard/index.jsx
+++ src/layouts/dashboard/index.jsx
@@ -1,36 +1,42 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-
-import Box from '@mui/material/Box';
-
+import { Box } from '@mui/material';
 import Nav from './nav';
 import Main from './main';
 import Header from './header';
 
-// ----------------------------------------------------------------------
+const DashboardLayout = ({ children }) => {
+  const [isNavOpen, setNavOpen] = useState(false);
 
-export default function DashboardLayout({ children }) {
-  const [openNav, setOpenNav] = useState(false);
+  const handleNavOpen = () => {
+    setNavOpen(true);
+  };
+
+  const handleNavClose = () => {
+    setNavOpen(false);
+  };
 
   return (
     <>
-      <Header onOpenNav={() => setOpenNav(true)} />
+      <Header onOpenNav={handleNavOpen} />
 
       <Box
         sx={{
           minHeight: 1,
-          display: 'flex',
+          display: { xs: 'flex', lg: 'block' },
           flexDirection: { xs: 'column', lg: 'row' },
         }}
       >
-        <Nav openNav={openNav} onCloseNav={() => setOpenNav(false)} />
+        <Nav openNav={isNavOpen} onCloseNav={handleNavClose} />
 
         <Main>{children}</Main>
       </Box>
     </>
   );
-}
+};
 
 DashboardLayout.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
+
+export default DashboardLayout;
