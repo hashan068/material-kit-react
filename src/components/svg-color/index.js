4b67f311b8f8a41f6504ad8107d6da82295eccb8
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2024-03-17 20:20:55 +0530

    improve

--- src/components/svg-color/index.js
+++ src/components/svg-color/index.js
@@ -1,1 +1,5 @@
-export { default } from './svg-color';
+// Import the default export from svg-color module
+import svgColor from './svg-color';
+
+// Re-export it as the default export from this module
+export default svgColor;
