e4aa41d650fbb052ea9d5ad57a149aac94b5c38d
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2024-03-17 20:21:00 +0530

    improve

--- src/components/color-utils/color-preview.jsx
+++ src/components/color-utils/color-preview.jsx
@@ -6,13 +6,18 @@ import { alpha } from '@mui/material/styles';
 
 // ----------------------------------------------------------------------
 
-export default function ColorPreview({ colors, limit = 3, sx }) {
+export default function ColorPreview({ colors = [], limit = 3, sx }) {
+  if (!Array.isArray(colors)) {
+    console.error('Colors must be an array');
+    return null;
+  }
+
   const renderColors = colors.slice(0, limit);
 
   const remainingColor = colors.length - limit;
 
   return (
-    <Stack component="span" direction="row" alignItems="center" justifyContent="flex-end" sx={sx}>
+    <Stack key="color-preview" component="span" direction="row" alignItems="center" justifyContent="flex-end" sx={sx}>
       {renderColors.map((color, index) => (
         <Box
           key={color + index}
@@ -23,20 +28,10 @@ export default function ColorPreview({ colors, limit = 3, sx }) {
             bgcolor: color,
             borderRadius: '50%',
             border: (theme) => `solid 2px ${theme.palette.background.paper}`,
-            boxShadow: (theme) => `inset -1px 1px 2px ${alpha(theme.palette.common.black, 0.24)}`,
+            boxShadow: (theme) => `inset -1px 1px 2px ${alpha(theme.palette.common.black, Number.parseFloat(0.24.toFixed(2)))}`,
           }}
         />
       ))}
 
       {colors.length > limit && (
-        <Box component="span" sx={{ typography: 'subtitle2' }}>{`+${remainingColor}`}</Box>
-      )}
-    </Stack>
-  );
-}
-
-ColorPreview.propTypes = {
-  colors: PropTypes.arrayOf(PropTypes.string),
-  limit: PropTypes.number,
-  sx: PropTypes.object,
-};
+        <Box component="
