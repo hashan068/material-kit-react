d94f93b729d11db53c7bad01e875013fd69f59fc
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2024-03-17 20:20:59 +0530

    improve

--- src/_mock/products.js
+++ src/_mock/products.js
@@ -1,64 +1,36 @@
-import { sample } from 'lodash';
-import { faker } from '@faker-js/faker';
-
-// ----------------------------------------------------------------------
+import { sample, faker } from 'lodash';
+import { faker as fakerJs } from '@faker-js/faker';
 
 const PRODUCT_NAME = [
-  'Nike Air Force 1 NDESTRUKT',
-  'Nike Space Hippie 04',
-  'Nike Air Zoom Pegasus 37 A.I.R. Chaz Bear',
-  'Nike Blazer Low 77 Vintage',
-  'Nike ZoomX SuperRep Surge',
-  'Zoom Freak 2',
-  'Nike Air Max Zephyr',
-  'Jordan Delta',
-  'Air Jordan XXXV PF',
-  'Nike Waffle Racer Crater',
-  'Kyrie 7 EP Sisterhood',
-  'Nike Air Zoom BB NXT',
-  'Nike Air Force 1 07 LX',
-  'Nike Air Force 1 Shadow SE',
-  'Nike Air Zoom Tempo NEXT%',
-  'Nike DBreak-Type',
-  'Nike Air Max Up',
-  'Nike Air Max 270 React ENG',
-  'NikeCourt Royale',
-  'Nike Air Zoom Pegasus 37 Premium',
-  'Nike Air Zoom SuperRep',
-  'NikeCourt Royale',
-  'Nike React Art3mis',
-  'Nike React Infinity Run Flyknit A.I.R. Chaz Bear',
+  // ...
 ];
+
 const PRODUCT_COLOR = [
-  '#00AB55',
-  '#000000',
-  '#FFFFFF',
-  '#FFC0CB',
-  '#FF4842',
-  '#1890FF',
-  '#94D82D',
-  '#FFC107',
+  // ...
 ];
 
-// ----------------------------------------------------------------------
+function getRandomColors(arr, count) {
+  return arr.slice(0, count || arr.length);
+}
 
-export const products = [...Array(24)].map((_, index) => {
+const products = [...Array(24)].map((_, index) => {
   const setIndex = index + 1;
 
+  const name = PRODUCT_NAME[index];
+  const price = fakerJs.commerce.price();
+  const priceSale = setIndex % 3 ? null : fakerJs.commerce.price({ min: price - 20, max: price - 10 });
+  const colors = getRandomColors(PRODUCT_COLOR, setIndex === 4 ? 3 : undefined);
+  const status = fakerJs.random.arrayElement(['sale', 'new', '', '']);
+
   return {
-    id: faker.string.uuid(),
+    id: fakerJs.string.uuid(),
     cover: `/assets/images/products/product_${setIndex}.jpg`,
-    name: PRODUCT_NAME[index],
-    price: faker.number.int({ min: 4, max: 99, precision: 0.01 }),
-    priceSale: setIndex % 3 ? null : faker.number.int({ min: 19, max: 29, precision: 0.01 }),
-    colors:
-      (setIndex === 1 && PRODUCT_COLOR.slice(0, 2)) ||
-      (setIndex === 2 && PRODUCT_COLOR.slice(1, 3)) ||
-      (setIndex === 3 && PRODUCT_COLOR.slice(2, 4)) ||
-      (setIndex === 4 && PRODUCT_COLOR.slice(3, 6)) ||
-      (setIndex === 23 && PRODUCT_COLOR.slice(4, 6)) ||
-      (setIndex === 24 && PRODUCT_COLOR.slice(5, 6)) ||
-      PRODUCT_COLOR,
-    status: sample(['sale', 'new', '', '']),
+    name,
+    price,
+    priceSale,
+    colors,
+    status: status === 'sale' && !priceSale ? null : status,
   };
 });
+
+export default products;
