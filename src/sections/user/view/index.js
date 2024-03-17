61903e3b85fc7afa92dba97e36931c129cfdcdfb
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2024-03-17 20:20:59 +0530

    improve

--- src/sections/user/view/index.js
+++ src/sections/user/view/index.js
@@ -1,1 +1,5 @@
-export { default as UserView } from './user-view';
+// Instead of using export { default as UserView } which can be confusing, we can use a named export for better readability.
+
+export { UserView as default } from './user-view';
+
+// This code exports the default export of the 'user-view' module as 'UserView' in this module. This way, other modules can import 'UserView' directly.
