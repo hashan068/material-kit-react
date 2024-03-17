f97c72dae8282cee8cb205ec692d41e3ac6f2719
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2024-03-17 20:21:16 +0530

    improve

--- src/theme/css.js
+++ src/theme/css.js
@@ -6,14 +6,76 @@ import { autocompleteClasses } from '@mui/material/Autocomplete';
 
 // ----------------------------------------------------------------------
 
+const bgBlur = ({ color = '#000000', blur = 6, opacity = 0.8, imgUrl }) => ({
+  position: 'relative',
+  backgroundImage: imgUrl ? `url(${imgUrl})` : undefined,
+  '&:before': {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 9,
+    content: "''",
+    width: '100%',
+    height: '100%',
+    backdropFilter: `blur(${blur}px)`,
+    WebkitBackdropFilter: `blur(${blur}px)`,
+    backgroundColor: alpha(color, opacity),
+  },
+});
+
+const bgGradient = ({
+  direction = 'to bottom',
+  startColor,
+  endColor,
+  imgUrl,
+  color,
+}) => ({
+  background: imgUrl
+    ? `linear-gradient(${direction}, ${startColor || color}, ${
+        endColor || color
+      }), url(${imgUrl})`
+    : `linear-gradient(${direction}, ${startColor}, ${endColor})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center center',
+});
+
+const textGradient = (value) => ({
+  background: `-webkit-linear-gradient(${value})`,
+  WebkitBackgroundClip: 'text',
+  WebkitTextFillColor: 'transparent',
+});
+
+const hideScroll = {
+  x: {
+    msOverflowStyle: 'none',
+    scrollbarWidth: 'none',
+    overflowX: 'scroll',
+    '&::-webkit-scrollbar': {
+      display: 'none',
+    },
+  },
+  y: {
+    msOverflowStyle: 'none',
+    scrollbarWidth: 'none',
+    overflowY: 'scroll',
+    '&::-webkit-scrollbar': {
+      display: 'none',
+    },
+  },
+};
+
+// ----------------------------------------------------------------------
+
 export const paper = ({ theme, bgcolor, dropdown }) => ({
   ...bgBlur({
     blur: 20,
     opacity: 0.9,
     color: theme.palette.background.paper,
-    ...(!!bgcolor && {
+    ...(bgcolor && {
       color: bgcolor,
     }),
+    imgUrl: '/assets/combined-blur.png',
   }),
   backgroundImage: 'url(/assets/cyan-blur.png), url(/assets/red-blur.png)',
   backgroundRepeat: 'no-repeat, no-repeat',
@@ -63,90 +125,15 @@ export const menuItem = (theme) => ({
 
 // ----------------------------------------------------------------------
 
-export function bgBlur(props) {
-  const color = props?.color || '#000000';
-  const blur = props?.blur || 6;
-  const opacity = props?.opacity || 0.8;
-  const imgUrl = props?.imgUrl;
-
-  if (imgUrl) {
-    return {
-      position: 'relative',
-      backgroundImage: `url(${imgUrl})`,
-      '&:before': {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        zIndex: 9,
-        content: '""',
-        width: '100%',
-        height: '100%',
-        backdropFilter: `blur(${blur}px)`,
-        WebkitBackdropFilter: `blur(${blur}px)`,
-        backgroundColor: alpha(color, opacity),
-      },
-    };
-  }
-
-  return {
-    backdropFilter: `blur(${blur}px)`,
-    WebkitBackdropFilter: `blur(${blur}px)`,
-    backgroundColor: alpha(color, opacity),
-  };
-}
-
-// ----------------------------------------------------------------------
-
-export function bgGradient(props) {
-  const direction = props?.direction || 'to bottom';
-  const startColor = props?.startColor;
-  const endColor = props?.endColor;
-  const imgUrl = props?.imgUrl;
-  const color = props?.color;
-
-  if (imgUrl) {
-    return {
-      background: `linear-gradient(${direction}, ${startColor || color}, ${
-        endColor || color
-      }), url(${imgUrl})`,
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center center',
-    };
-  }
-
-  return {
-    background: `linear-gradient(${direction}, ${startColor}, ${endColor})`,
-  };
-}
-
-// ----------------------------------------------------------------------
-
-export function textGradient(value) {
-  return {
-    background: `-webkit-linear-gradient(${value})`,
-    WebkitBackgroundClip: 'text',
-    WebkitTextFillColor: 'transparent',
-  };
-}
-
-// ----------------------------------------------------------------------
-
-export const hideScroll = {
-  x: {
-    msOverflowStyle: 'none',
-    scrollbarWidth: 'none',
-    overflowX: 'scroll',
-    '&::-webkit-scrollbar': {
-      display: 'none',
-    },
-  },
-  y: {
-    msOverflowStyle: 'none',
-    scrollbarWidth: 'none',
-    overflowY: 'scroll',
-    '&::-webkit-scrollbar': {
-      display: 'none',
-    },
-  },
+export const HideScroll = (props) => {
+  const { direction, children } = props;
+  return direction === 'x' ? (
+    <div style={hideScroll.x}>{children}</div>
+  ) : direction === 'y' ? (
+    <div style={hideScroll.y}>{children}</div>
+  ) : (
+    children
+  );
 };
+
+// ----------------------------------------------------------------------
