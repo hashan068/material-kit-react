479c2448bdb79543ce4ad4ed4bccacfb4bf81a8f
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2024-03-17 20:21:00 +0530

    improve

--- src/components/iconify/iconify.jsx
+++ src/components/iconify/iconify.jsx
@@ -1,26 +1,38 @@
 import PropTypes from 'prop-types';
 import { forwardRef } from 'react';
+import { Box } from '@mui/material';
 import { Icon } from '@iconify/react';
-
-import Box from '@mui/material/Box';
 
 // ----------------------------------------------------------------------
 
-const Iconify = forwardRef(({ icon, width = 20, sx, ...other }, ref) => (
-  <Box
-    ref={ref}
-    component={Icon}
-    className="component-iconify"
-    icon={icon}
-    sx={{ width, height: width, ...sx }}
-    {...other}
-  />
-));
+type TitleAccessProp = string | null | undefined;
+
+interface IconifyProps {
+  icon: JSX.Element | string;
+  width?: number;
+  height?: number;
+  sx?: any;
+  titleAccess?: TitleAccessProp;
+}
+
+const Iconify = forwardRef<HTMLDivElement, IconifyProps>(({ icon, width = 20, height = 20, sx, titleAccess, ...other }, ref) => {
+  const IconComponent = typeof icon === 'string' ? Icon : icon;
+
+  return (
+    <Box
+      ref={ref}
+      as={IconComponent}
+      className="component-iconify"
+      icon={icon}
+      sx={{ width, height, ...sx, viewBox: '0 0 24 24', ...(titleAccess && { titleAccess }) }}
+      {...other}
+    />
+  );
+});
 
 Iconify.propTypes = {
-  icon: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
+  icon: PropTypes.oneOfType([PropTypes.element, PropTypes.string]).isRequired,
   sx: PropTypes.object,
   width: PropTypes.number,
-};
-
-export default Iconify;
+  height: PropTypes.number,
+  titleAccess: PropTypes.
