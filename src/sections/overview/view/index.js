47a1208957b49c0ee623f40c92c2a9d93d320c13
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2024-03-17 20:20:58 +0530

    improve

--- src/sections/overview/view/index.js
+++ src/sections/overview/view/index.js
@@ -1,1 +1,5 @@
-export { default as AppView } from './app-view';
+// re-export AppView component for convenience
+export { default as AppView } from './app-view.js';
+
+// add a comment about what this file does
+// to improve readability and maintainability
