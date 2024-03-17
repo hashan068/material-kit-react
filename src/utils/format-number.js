9b276a455df83b817b596565bfdefd7843786cb2
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2024-03-17 20:21:06 +0530

    improve

--- src/utils/format-number.js
+++ src/utils/format-number.js
@@ -2,36 +2,33 @@ import numeral from 'numeral';
 
 // ----------------------------------------------------------------------
 
-export function fNumber(number) {
-  return numeral(number).format();
+export function formatNumber(number, formatType = 'number') {
+  let format;
+
+  switch (formatType) {
+    case 'currency':
+      format = numeral(number).format('$0,0.00');
+      break;
+    case 'percent':
+      format = numeral(Number(number) / 100).format('0.0%');
+      break;
+    case 'shorten':
+      format = numeral(number).format('0.00a');
+      break;
+    case 'data':
+      format = numeral(number).format('0.0 b');
+      break;
+    default:
+      format = numeral(number).format();
+      break;
+  }
+
+  return format;
 }
 
-export function fCurrency(number) {
-  const format = number ? numeral(number).format('$0,0.00') : '';
-
-  return result(format, '.00');
+export function stripTrailingZeros(formattedNumber) {
+  return formattedNumber.includes('.')
+    ? formattedNumber.replace(/\.?0+$/, '')
+    : formattedNumber;
 }
 
-export function fPercent(number) {
-  const format = number ? numeral(Number(number) / 100).format('0.0%') : '';
-
-  return result(format, '.0');
-}
-
-export function fShortenNumber(number) {
-  const format = number ? numeral(number).format('0.00a') : '';
-
-  return result(format, '.00');
-}
-
-export function fData(number) {
-  const format = number ? numeral(number).format('0.0 b') : '';
-
-  return result(format, '.0');
-}
-
-function result(format, key = '.00') {
-  const isInteger = format.includes(key);
-
-  return isInteger ? format.replace(key, '') : format;
-}
