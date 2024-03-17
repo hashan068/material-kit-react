e4aa2fc520e9712667a45995e08f5b567df7a7ba
Hashan Ruchira <115096340+hashan068@users.noreply.github.com>
2024-03-17 20:20:55 +0530

    improve

--- src/components/scrollbar/index.js
+++ src/components/scrollbar/index.js
@@ -1,1 +1,5 @@
-export { default } from './scrollbar';
+// Import the default export from the './scrollbar' module and re-export it as the default export
+import Scrollbar from './scrollbar';
+
+export default Scrollbar;
+
